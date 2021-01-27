6b5045bba1e6659f79196c4ed9d3c464e9918e35
ardit96 <Ardit.Hoti@student.cacttus.education>
2021-01-28 00:35:42 +0100

    Finished cateogry page

---
+++ front/categorypage.php
@@ -0,0 +1,65 @@
+<?php include "../front/header.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../front/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+    <h3 class="text-left category_name">Category 1</h3>
+    <div class="news_photos_title">
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <h4 class="text-center">"Title example"</h4>
+    </div>
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <h4 class="text-center">"Title example"</h4>
+    </div>
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <h4 class="text-center">"Title example"</h4>
+    </div>
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <h4 class="text-center">"Title example"</h4>
+    </div>
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <h4 class="text-center">"Title example"</h4>
+    </div>
+    </div>
+
+    <div class="other_news">
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <h2 class="title_other_news">Title Example</h2>
+    </div>
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <h2 class="title_other_news">Title Example</h2>
+    </div>
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <h2 class="title_other_news">Title Example</h2>
+    </div>
+    <div>
+        <img src="vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <h2 class="title_other_news">Title Example</h2>
+    </div>
+    </div>
+    </div>
+
+    
+    
+</body>
+
+
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<<<<<<< Updated upstream
 <?php
     header("Location: front/");
 ?>
+=======
+<?php include './front/header.php';?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/arberas-detyre-klase-basic-web/basic-web-development-website-and-cms/front/style.css">
+    <title>Rilindja</title>
+</head>
+<html>
+<body>
+<h1 class="category_title">Category 1</h1>
+    <div class="container">
+        <div class="card">
+            <img class="card_image" src="./front/vaccine.jpg">
+                <div class="post-title">
+                    The title of the post
+                </div>
+        </div>
+    </div>
+</body>
+
+</html>
+>>>>>>> Stashed changes
