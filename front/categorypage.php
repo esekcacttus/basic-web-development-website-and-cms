2d0ad6502ec8d992a0f432cb898ee25b506ff04b
Ardit96 <Ardit.Hoti@student.cacttus.education>
2021-02-01 20:09:14 +0100

    updated links

--- front/categorypage.php
+++ front/categorypage.php
@@ -1,4 +1,4 @@
-<?php include "../front/header.php"; ?>
+<?php include "header.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,49 +9,49 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="../front/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container-fluid">
     <h3 class="text-left category_name">Category 1</h3>
     <div class="news_photos_title">
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
         <h4 class="text-center">"Title example"</h4>
     </div>
     <div>
-        <img src="../front/images/vaccine.jpg"alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <img src="images/vaccine.jpg"alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
         <h4 class="text-center">"Title example"</h4>
     </div>
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
         <h4 class="text-center">"Title example"</h4>
     </div>
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
         <h4 class="text-center">"Title example"</h4>
     </div>
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="200px" height="150px" class="img-fluid" >
         <h4 class="text-center">"Title example"</h4>
     </div>
     </div>
 
     <div class="other_news">
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
         <h2 class="title_other_news">Title Example</h2>
     </div>
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
         <h2 class="title_other_news">Title Example</h2>
     </div>
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
         <h2 class="title_other_news">Title Example</h2>
     </div>
     <div>
-        <img src="../front/images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
+        <img src="images/vaccine.jpg" alt="foto vaksina" width="100px" height="100px">
         <h2 class="title_other_news">Title Example</h2>
     </div>
     </div>
