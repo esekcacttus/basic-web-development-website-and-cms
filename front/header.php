8f21de257222414ec5fa50ce611a642d72e01543
egzonz1 <egzon.zylfijaj@student.cacttus.education>
2021-02-01 20:04:12 +0100

    Changes on header of single page

---
+++ front/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../style.css">
+    <title>Rilindja</title>
+</head>
+<nav>
+    <div>
+        <img class="logo_img" src="./rilindjaLogo.svg" />
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="./category1.php">Category 1</a>
+        </li>
+        <li>
+            <a href="./category2.php">Category 2</a>
+        </li>
+        <li>
+            <a href="./category3.php">Category 3</a>
+        </li>
+        <li>
+            <a href="./category4.php">Category 4</a>
+        </li>
+    </ul>
+</nav>
+
+<body>
--- front/single_page.php
+++ front/single_page.php
@@ -1,5 +1,16 @@
+<?php include "../front/header.php"; ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../front/style.css">
+</head>
 <body>
 <div id="preloader">
   <div id="status">&nbsp;</div>
@@ -37,8 +48,8 @@
       <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav main_nav">
           <li class="active"><span class="fa fa-home desktop-home"></span><span class="mobile-show">Home</span></a></li>
-          <li><a href="#">Technology</a></li>
-          <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mobile</a>
+          <li><a href="#">Category1</a></li>
+          <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Category1</a>
           </li>
         </ul>
       </div>
@@ -47,19 +58,6 @@
   <section id="newsSection">
     <div class="row">
       <div class="col-lg-12 col-md-12">
-    
-          <div class="social_area">
-            <ul class="social_nav">
-              <li class="facebook"></a></li>
-              <li class="twitter"></a></li>
-              <li class="flickr"></a></li>
-              <li class="pinterest"></li>
-              <li class="googleplus"></li>
-              <li class="vimeo"></li>
-              <li class="youtube"></li>
-              <li class="mail"></li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
@@ -71,14 +69,13 @@
           <div class="single_page">
             <ol class="breadcrumb">
               <li>Home</li>
-              <li>Technology</li>
-              <li class="active">Mobile</li>
+              <li>Category</li>
+              <li class="active">Category1</li>
             </ol>
             <h1>welcome the new iPhone 13 concept</h1>
-            <div class="single_page_content"> <img class="img-center" src="../images/single_post_img.jpg" alt="">
+            <div class="single_page_content"> <img class="img-center" src="images/single_post_img.jpg" alt="">
               <p>Nunc tincidunt, elit non cursus euismod, lacus augue ornare metus, egestas imperdiet nulla nisl quis mauris. Suspendisse a pharetra urna. Morbi dui lectus, pharetra nec elementum eget, vulputate ut nisi. Aliquam accumsan, nulla sed feugiat vehicula, lacus justo semper libero, quis porttitor turpis odio sit amet ligula. Duis dapibus fermentum orci, nec malesuada libero vehicula ut. Integer sodales, urna eget interdum eleifend, nulla nibh laoreet nisl, quis dignissim mauris dolor eget mi. Donec at mauris enim. Duis nisi tellus, adipiscing a convallis quis, tristique vitae risus. Nullam molestie gravida lobortis. Proin ut nibh quis felis auctor ornare. Cras ultricies, nibh at mollis faucibus, justo eros porttitor mi, quis auctor lectus arcu sit amet nunc. Vivamus gravida vehicula arcu, vitae vulputate augue lacinia faucibus.</p>
               <blockquote> Donec volutpat nibh sit amet libero ornare non laoreet arcu luctus. Donec id arcu quis mauris euismod placerat sit amet ut metus. Sed imperdiet fringilla sem eget euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque adipiscing, neque ut pulvinar tincidunt, est sem euismod odio, eu ullamcorper turpis nisl sit amet velit. Nullam vitae nibh odio, non scelerisque nibh. Vestibulum ut est augue, in varius purus. </blockquote>
             </div>
-        
 </body>
 </html>
