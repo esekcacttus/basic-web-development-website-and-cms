66631a54099574ab230681f2974bf902554eb9cb
ArberCacttus <arber.asllani@student.cacttus.education>
2021-02-03 19:48:28 +0100

    Show categories on header

--- front/db.php
+++ front/db.php
@@ -1,23 +1,19 @@
 <?php
 // define connectoin variables
-$host   = "127.0.0.1"; // 127.0.0.1
+$host   = "localhost"; // 127.0.0.1
 $user       = "root";
 $password   = "";
 $dbName     = "portali";
+$port = 3308;
 
 // create connection
 $dbConnection = null;
 try {
-    $dbConnection = new PDO(
-        'mysql:host=' . $host . ';dbname=' . $dbName,
-        $user,
-        $password
-    );
-} catch (Exception $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
+    $dbConnection = new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $dbName, $user, $password);
+} catch (PDOException $e) {
+    echo "Error!: " . $e->getMessage() . "<br/>";
+    exit();
 }
-
 if (!$dbConnection) {
     echo "No database connection!";
     die();
