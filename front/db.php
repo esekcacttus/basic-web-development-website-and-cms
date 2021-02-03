f06b35e8ea15ab85d5f3b2f3b3c0edf705bd8a3b
ArberCacttus <arber.asllani@student.cacttus.education>
2021-02-03 19:48:54 +0100

    Revert DB.php

--- front/db.php
+++ front/db.php
@@ -1,19 +1,23 @@
 <?php
 // define connectoin variables
-$host   = "localhost"; // 127.0.0.1
+$host   = "127.0.0.1"; // 127.0.0.1
 $user       = "root";
 $password   = "";
 $dbName     = "portali";
-$port = 3308;
 
 // create connection
 $dbConnection = null;
 try {
-    $dbConnection = new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $dbName, $user, $password);
-} catch (PDOException $e) {
-    echo "Error!: " . $e->getMessage() . "<br/>";
-    exit();
+    $dbConnection = new PDO(
+        'mysql:host=' . $host . ';dbname=' . $dbName,
+        $user,
+        $password
+    );
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
 }
+
 if (!$dbConnection) {
     echo "No database connection!";
     die();
