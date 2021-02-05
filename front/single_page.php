b5761455c0bfe7d34f72b7dcfacfe74f301e37ae
egzonz1 <egzon.zylfijaj@student.cacttus.education>
2021-02-05 19:50:28 +0100

    Changes

--- front/single_page.php
+++ front/single_page.php
@@ -1,5 +1,4 @@
 <?php include "../front/header.php"; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,51 +6,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="../front/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div id="preloader">
-  <div id="status">&nbsp;</div>
-</div>
-<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
-<div class="container">
-  <header id="header">
-    <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12">
-        <div class="header_top">
-          <div class="header_top_left">
-        </div>
-      </div>
-      <div class="col-lg-12 col-md-12 col-sm-12">
-        <div class="header_bottom">
-        </div>
-      </div>
-    </div>
-  </header>
-  
-  <section id="newsSection">
-    <div class="row">
-      <div class="col-lg-12 col-md-12">
-        </div>
-      </div>
-    </div>
-  </section>
+<div class="single_container">
+ 
   <section id="contentSection">
     <div class="row">
       <div class="col-lg-8 col-md-8 col-sm-8">
-        <div class="left_content">
+        <div style="text-align">
           <div class="single_page">
             <ol class="breadcrumb">
               <li>Home</li>
               <li>Category</li>
               <li class="active">Category1</li>
             </ol>
-            <h1>welcome the new iPhone 13 concept</h1>
-            <div class="single_page_content">
-               <img class="img-center" src="images/single_post_img.jpg" alt="">
+            <h1>Welcome the new iPhone 13 concept</h1> 
+            <div class="single_container">
+               <img class="img-center" src="images/single_post.jpg" alt="">
               <p>Nunc tincidunt, elit non cursus euismod, lacus augue ornare metus, egestas imperdiet nulla nisl quis mauris. Suspendisse a pharetra urna. Morbi dui lectus, pharetra nec elementum eget, vulputate ut nisi. Aliquam accumsan, nulla sed feugiat vehicula, lacus justo semper libero, quis porttitor turpis odio sit amet ligula. Duis dapibus fermentum orci, nec malesuada libero vehicula ut. Integer sodales, urna eget interdum eleifend, nulla nibh laoreet nisl, quis dignissim mauris dolor eget mi. Donec at mauris enim. Duis nisi tellus, adipiscing a convallis quis, tristique vitae risus. Nullam molestie gravida lobortis. Proin ut nibh quis felis auctor ornare. Cras ultricies, nibh at mollis faucibus, justo eros porttitor mi, quis auctor lectus arcu sit amet nunc. Vivamus gravida vehicula arcu, vitae vulputate augue lacinia faucibus.</p>
               <blockquote> Donec volutpat nibh sit amet libero ornare non laoreet arcu luctus. Donec id arcu quis mauris euismod placerat sit amet ut metus. Sed imperdiet fringilla sem eget euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque adipiscing, neque ut pulvinar tincidunt, est sem euismod odio, eu ullamcorper turpis nisl sit amet velit. Nullam vitae nibh odio, non scelerisque nibh. Vestibulum ut est augue, in varius purus. </blockquote>
             </div>
